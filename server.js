cfda6bad36c42b8347ecad790a945a76ac5b9bc3
GabrielWelvaert <gabewelvaert@gmail.com>
2024-10-29 12:08:26 -0400

    stuff

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const PORT = 3000;
 // gives server accesss to static JS and CSS files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// server URLs
 app.get('/', (req,res) => {
-    res.sendFile('views/landing.html', {root: __dirname});
+    res.sendFile(path.join(__dirname, 'views', 'landing.html'));
 });
 
+// start the server
 app.listen(PORT, () => {
 
 })
