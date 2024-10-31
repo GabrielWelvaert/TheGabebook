10573658dae39e23f6aeeeead8fcad1ae0c2f198
GabrielWelvaert <gabewelvaert@gmail.com>
2024-10-31 11:22:30 -0400

    login button interfaces DB

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,21 +2,53 @@ const UserModel = require("../models/UserModel.js")
 
 const UserController = {
     async registerUser(req, res){
-        console.log("entered registerUser() in UserController.js")
-        const { firstName, lastName, email, password, birthday } = req.body;
-        if(!firstName || !lastName || !email || !password || !birthday){
-            return res.status(400).json({success: false, message:"All fields are required"});
-        }
+        const { firstName, lastName, email, password, confirmedPassword, birthday } = req.body;
         try {
+
+            // check if any forms were left empty
+            if(!firstName || !lastName || !email || !password || !birthday){
+                return res.status(400).json({success: false, message:"All fields are required"});
+            }
+
+            // check if this email is already registered
             const existingUser = await UserModel.findUserByEmail(email);
-            if (existingUser){
+            if(existingUser){
                 return res.status(400).json({success: false, message: "Email already registered"});
             }
 
+            // check if password and confirmed password match
+            if(confirmedPassword !== password){
+                return res.status(400).json({success: false, message:"Passwords do not match"});
+            }
+
+            // register new user if no issues found!
             const newUser = await UserModel.createUser(req.body);
             return res.status(201).json({success: true, message:"User registered successfully", user: newUser}); 
         } catch (error){
             console.error("Error registering user: ", error);
+            return res.status(500).json({success: false, message: "Server Error"});
+        }
+    },
+    async loginUser(req, res){
+        const {email,password} = req.body;
+        try{
+            // check if any forms are empty
+            if(!email || !password){
+                return res.status(400).json({success: false, message:"All fields are required"});
+            }
+            // check if this email is registered
+            const existingUser = await UserModel.findUserByEmail(email);
+            if(!existingUser){
+                return res.status(400).json({success: false, message: "Email not registered; Sign-Up below"});
+            }
+            // todo check if password was incorrect
+            const correctPassword = await UserModel.validatePassword(email, password);
+            if(correctPassword){
+                return res.status(200).json({success: true, message:"Successful login"}); 
+            } else {
+                return res.status(401).json({success: false, message: "Incorrect password"});
+            }
+        }catch (error){
             return res.status(500).json({success: false, message: "Server Error"});
         }
     }
--- models/UserModel.js
+++ models/UserModel.js
@@ -2,23 +2,27 @@ const db = require('../config/db.js')
 
 const UserModel = {
     async findUserByEmail(email){
-        console.log("entered findUserByEmail() in UserModel.js");
         const query = 'SELECT * FROM users WHERE email = ?';
         const values = [email];
-        const [rows] = await db.promise().query(query, values);
-        return rows.length > 0;
+        const [rows,fields] = await db.promise().query(query, values);
+        return rows[0] ? rows[0] : undefined;
     },
 
     async createUser(userData){
-        console.log("entered createUser() in UserModel.js");
         const { firstName, lastName, email, password, birthday } = userData;
-        const query = `
-            INSERT INTO users (firstName, lastName, email, password, birthday)
-            VALUES (?,?,?,?,?);
-        `;
+        const query = `INSERT INTO users (firstName, lastName, email, password, birthday) VALUES (?,?,?,?,?);`;
         const values = [firstName, lastName, email, password, birthday];
-        const result = await db.promise().query(query, values);
-        // return result.rows[0];
+        const [rows,fields] = await db.promise().query(query, values);
+        return rows[0] ? rows[0] : undefined;
+    },
+
+    // this function assumes we have already validated that email is registered
+    // returns true or false
+    async validatePassword(email, password){
+        const values = [email];
+        const query = `SELECT password FROM users WHERE email = ?`
+        const [rows,fields] = await db.promise().query(query, values);
+        return rows[0] ? rows[0].password === password : undefined; 
     }
 }
 
