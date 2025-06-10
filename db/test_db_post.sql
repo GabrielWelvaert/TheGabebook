50b89e98581bcdbb3cae9b16a52e22c008b94a5d
Gabriel Welvaert <Gabewelvaert@gmail.com>
2025-06-09 20:34:13 -0400

    update database schema structure dump

--- db/test_db_post.sql
+++ db/test_db_post.sql
@@ -27,14 +27,13 @@ CREATE TABLE `post` (
   `postUUID` binary(16) NOT NULL,
   `authorId` int unsigned DEFAULT NULL,
   `text` varchar(1000) DEFAULT NULL,
-  `media` varchar(2048) DEFAULT NULL,
   `datetime` datetime DEFAULT NULL,
   PRIMARY KEY (`postId`),
   UNIQUE KEY `postId_UNIQUE` (`postId`),
   UNIQUE KEY `postUUID_UNIQUE` (`postUUID`),
   KEY `authorId_idx` (`authorId`),
   CONSTRAINT `authorId` FOREIGN KEY (`authorId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=159 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=205 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -46,4 +45,4 @@ CREATE TABLE `post` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-07 10:15:09
+-- Dump completed on 2025-06-09 20:31:43
